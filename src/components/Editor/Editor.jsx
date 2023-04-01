a3bd6367a3f154b8fc48db3fcbe51b96540d6b8e
deleonroselle21 <deleonroselle21@gmail.com>
2023-04-01 13:32:16 -0400

    initial commit

---
+++ src/components/Editor/Editor.jsx
@@ -0,0 +1,33 @@
+import '../Editor/editor.css'
+import { useState } from 'react';
+import EditorComponent from '../EditorComponent/EditorComponent';
+
+function Editor() {
+    const [html,setHtml]=useState('');
+    const [css,setCss]=useState('');
+    const [javascript,setJavascript]=useState('');
+
+    const srcDoc=`<html><style>${css}</style><body>${html}</body><script>${javascript}<script>
+    <html>`
+
+  return (
+<div className='main'>
+  <div className='editor__container'>
+   <EditorComponent language='xml' displayName='HTML'  value={html} onChange={setHtml} />
+   <EditorComponent language='css' displayName='CSS' value={css} onChange={setCss} />
+   <EditorComponent language='javascript' displayName='JAVASCRIPT' value={javascript} onChange={setJavascript} />
+   
+  </div>
+
+   <div className='output__container'>
+   <iframe  srcDoc={srcDoc} title="output" sandbox='allow-scripts' width="100%" height="100%" frameborder='0'  >
+      
+   </iframe>
+    </div>
+</div>
+
+
+  );
+}
+
+export default Editor;
---
+++ src/components/EditorComponent/EditorComponent.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../EditorComponent/editorComponent.css'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import {Controlled as ControlledEditor} from 'react-codemirror2'
+
+
+
+const EditorComponent = (props) => {
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+    } = props
+    function handleChange(editor,data,value){
+        onChange(value);
+    }
+  return (
+    <div className='editComponent__container'>
+        <div className="editor-title">
+            {displayName}
+        </div>
+        <div>
+            <ControlledEditor 
+            onBeforeChange={handleChange}
+            value={value}
+            className='code-mirror-wrapper controlled-editor'
+           
+            options={{
+                lineWrapping:true,
+                theme:'material',
+                lint:true,
+                mode:language,
+                lineNumbers:true
+
+            }}/>
+        </div>
+    </div>
+  )
+}
+
+export default EditorComponent
