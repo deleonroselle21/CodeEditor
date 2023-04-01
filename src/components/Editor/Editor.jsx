37addb0a5a04733165d23c768a82ee273ed82db5
deleonroselle21 <deleonroselle21@gmail.com>
2023-04-01 14:31:47 -0400

    edit editor file

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -12,14 +12,18 @@ function Editor() {
 
   return (
 <div className='main'>
+    
+  <div >
+  <h2>Code</h2>
   <div className='editor__container'>
    <EditorComponent language='xml' displayName='HTML'  value={html} onChange={setHtml} />
    <EditorComponent language='css' displayName='CSS' value={css} onChange={setCss} />
    <EditorComponent language='javascript' displayName='JAVASCRIPT' value={javascript} onChange={setJavascript} />
-   
+   </div>
   </div>
-
+  
    <div className='output__container'>
+   <h2>Output</h2>
    <iframe  srcDoc={srcDoc} title="output" sandbox='allow-scripts' width="100%" height="100%" frameborder='0'  >
       
    </iframe>
